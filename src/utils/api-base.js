2eaa1b88a3e06be6876edb2c65b340552dbb2409
Guilherme Parente <guilherme.parente17@outlook.com>
2022-07-08 13:08:25 -0300

    Segurança no env

--- src/utils/api-base.js
+++ src/utils/api-base.js
@@ -8,8 +8,7 @@ const api = axios.create({
   baseURL: `https://api.github.com/`,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer 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`,
-    // 'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+    'Authorization': `Bearer ${process.env.REACT_APP_API_TOKEN}`,
     Accept: 'application/json',
   },
 })
